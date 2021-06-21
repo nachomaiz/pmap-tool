4bba0be2a7a2e1676bb535ff69674c611283ecf3
Nacho Maiz <ignacio.maiz@gmail.com>
2021-06-21 20:42:23 +0100

    handling edge cases and caching data

--- app.py
+++ app.py
@@ -12,19 +12,27 @@ mplparams = {
     'axes.facecolor' : '#0E1117',
     'axes.edgecolor' : '#FAFAFA',
     'axes.labelcolor' : '#FAFAFA',
-    'axes.grid' : False,
     'xtick.color' : '#FAFAFA',
     'xtick.labelcolor' : '#FAFAFA',
     'ytick.color' : '#FAFAFA',
     'ytick.labelcolor' : '#FAFAFA',
-    'axes.prop_cycle' : cycler.cycler('color',plt.cm.Dark2(range(0,5))),
+    'axes.prop_cycle' : cycler.cycler('color',plt.cm.Dark2(range(0,4))),
     'font.size' : '14.0'
 }
 
 st.set_page_config(
     page_title="Perceptual Map Tool",
-    page_icon="🧭"
+    page_icon="💡"
 )
+
+# Cached data and model load for faster interactivity
+@st.cache(allow_output_mutation=True)
+def build_pmap_model(X, supp, n_components, n_iter):
+    return pmap.PMAP(n_components, n_iter).fit(X, supp)
+
+@st.cache()
+def load_data(data):
+    return pd.read_excel(data, index_col=0)
 
 """
 # Perceptual Map Tool
@@ -46,7 +54,7 @@ uploaded_file = st.sidebar.file_uploader(
     type=['xls','xlsx']
 )
 
-# sample data functionality which displays filename in sidebar
+# sample data functionality which displays filename in sidebar, overrides file upload for referencing.
 if sample := st.checkbox('Load sample data'):
     uploaded_file = 'data/brand data.xlsx'
     filename = uploaded_file.split('/')[-1]
@@ -57,22 +65,60 @@ if sample := st.checkbox('Load sample data'):
 
 # Main app once data is ready
 if uploaded_file:
-    data = pd.read_excel(uploaded_file, index_col=0)
-    n_cols = data.shape[1]
+
+    # handles most cases of bad files.
+    try:
+        data = load_data(uploaded_file)
+        for c in data.columns:
+            if data[c].dtype not in ('float','int'):
+                st.stop()
+    except:
+        st.error("Your file could not be loaded.")
+        st.stop()
+    
+    n_rows, n_cols = data.shape
+
+    # handles data being too small
+    if n_rows < 3 or n_cols < 3:
+        st.error("Data is too small for perceptual map. Needs at least 3 rows and columns.")
+        st.stop()
     with st.beta_expander("Show Data"):
         st.dataframe(data)
+
+    # Warning for small datasets
+    if n_rows < 4 or n_cols < 4:
+        st.sidebar.warning("Your data can be turned into a perceptual map, but it only has 3 {dim}, so you cannot pick {comps} supplementary {dim}."
+            .format(dim="rows" if n_rows < 4 else "columns",
+                    comps="components or" if n_cols < 4 else ""
+                    )
+                )
         
     # Model parameters
-    n_components = st.sidebar.slider("Number of components (default 2)", 2, 10 if n_cols >= 10 else n_cols - 1, 2, help="For performance reasons the max is 10. Allows one less than the total number of data columns")
+    if n_cols > 3:
+        n_components = st.sidebar.slider("Number of components (default 2)", 2, 10 if n_cols > 10 else n_cols - 1, 2, help="For performance reasons the max is 10. Allows one less than the total number of data columns")
+    else: 
+        n_components = 2
+        st.sidebar.info("Number of components = 2")
     x_component = st.sidebar.slider("Horizontal component", 0, n_components - 1, 0)
     y_component = st.sidebar.slider("Vertical component", 0, n_components - 1, 1)
     
     # Supplementary Data
     if st.sidebar.checkbox("Supplementary data", help="For plotting grouped averages, factors, etc. Only the final rows and columns can be supplementary"):
-        supp = (
-            st.sidebar.slider("Supplementary rows", 0, data.shape[0] - 3, 0, 1, help="Must leave at least 3 rows as core data"),
-            st.sidebar.slider("Supplementary columns", 0, n_cols - 3, 1 if sample else 0, 1, help="Must leave at least 3 columns as core data") # default value is 1 if sample data is loaded
-        )
+        
+        # Show only if each data shape dimension is large enough
+        if n_rows > 3:
+            s_rows = st.sidebar.slider("Supplementary rows", 0, n_rows - 3, 0, 1, help="Must leave at least 3 rows as core data")
+        else: 
+            s_rows = 0
+            st.sidebar.info("Supplementary rows not available.")
+        if n_cols > 3:
+            s_cols = st.sidebar.slider("Supplementary columns", 0, n_cols - 3, 1 if sample else 0, 1, help="Must leave at least 3 columns as core data") # default value is 1 if sample data is loaded
+        else: 
+            s_cols = 0
+            st.sidebar.info("Supplementary columns not available.")
+        
+        supp = (s_rows,s_cols)
+
         plot_supp = st.sidebar.selectbox("Plot supplementary data",[True,False,'only'])
     else:
         supp = None
@@ -91,13 +137,7 @@ if uploaded_file:
     # Parse invert axis for PMAP functions
     invert_ax = 'b' if invert_x and invert_y else 'x' if invert_x else 'y' if invert_y else None
 
-    # Initialize and fit PMAP
-    @st.cache(allow_output_mutation=True)
-    def build_pmap_model(n_components, n_iter):
-        return pmap.PMAP(n_components=n_components, n_iter=n_iter)
-
-    model = build_pmap_model(n_components, n_iter)
-    model = model.fit(data, supp=supp)
+    model = build_pmap_model(data, supp, n_components, n_iter)
 
     # Plot Perceptual Map
     with plt.style.context(mplparams):
@@ -115,4 +155,4 @@ if uploaded_file:
         st.markdown(utils.download_button(out_data, 'pmap-output.xlsx', 'Download output as excel'), unsafe_allow_html=True)
         st.dataframe(out_data)
 
-st.info("Developed with ❤ by [nachomaiz](https://github.com/nachomaiz) based on the [prince](https://github.com/MaxHalford/prince) package")
+st.info("Developed with ❤ by [nachomaiz](https://github.com/nachomaiz) based on the [prince](https://github.com/MaxHalford/prince) package. [GitHub repo](https://github.com/nachomaiz/pmap).")
